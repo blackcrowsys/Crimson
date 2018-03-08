2d649b470e307610920fc03e606aceee128ca513
ramindurs <ramindur.singh@blackcrowsys.com>
2018-03-08 20:01:46 +0530

    First commit for base matrix class

---
+++ src/main/scala/com/blackcrowsys/crimson/matrix/Matrix.scala
@@ -0,0 +1,30 @@
+package com.blackcrowsys.crimson.matrix
+
+object Matrix {
+
+  class Matrix[T](
+                   val contents: Array[T],
+                   val columns: Int
+                 ) {
+    def get(row: Int, col: Int): T = {
+      if (col > columns) {
+        throw new IllegalArgumentException("Column Index exceeds number of columns: " + col)
+      }
+      if (row > contents.length / columns) {
+        throw new IllegalArgumentException("Row Index exceeds number of rows: " + row)
+      }
+      val subtractor = if (row == 1 && col == 1) 1
+      else if (col == 1) 0
+      else 1
+      contents(row * col - subtractor)
+    }
+
+  }
+
+
+  def create(contents: Array[Double], columns: Int): Matrix[Double] = {
+    if (contents.length % columns != 0 || contents.length < columns)
+      throw new IllegalArgumentException("Contents mismatch")
+    new Matrix[Double](contents, columns)
+  }
+}
---
+++ src/test/scala/com/blackcrowsys/crimson/matrix/MatrixTests.scala
@@ -0,0 +1,38 @@
+package com.blackcrowsys.crimson.matrix
+
+import org.scalatest.{FunSuite}
+
+class MatrixTests extends FunSuite {
+
+  val x = Matrix.create(Array(12.2, 4, 7, 10), 2)
+
+  test("A double matrix with 2 columns and 2 rows") {
+    val ele1 = x.get(1, 1)
+    assert(ele1 == 12.2)
+
+    val ele2 = x.get(1, 2)
+    assert(ele2 == 4)
+
+    val ele3 = x.get(2, 1)
+    assert(ele3 == 7)
+
+    val ele4 = x.get(2, 2)
+    assert(ele4 == 10)
+  }
+
+  assertThrows[IllegalArgumentException]{
+    val e = x.get(1,3)
+  }
+
+  assertThrows[IllegalArgumentException]{
+    val e = x.get(3, 1)
+  }
+
+  assertThrows[IllegalArgumentException]{
+    val y = Matrix.create(Array(12.2, 4, 7), 2)
+  }
+
+  assertThrows[IllegalArgumentException]{
+    val y = Matrix.create(Array(12.1), 2)
+  }
+}
