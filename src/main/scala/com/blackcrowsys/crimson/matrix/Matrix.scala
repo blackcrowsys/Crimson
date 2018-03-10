0da06eac305b4248aa41bb970470e91022d5ea5f
ramindurs <ramindur.singh@blackcrowsys.com>
2018-03-10 18:09:01 +0530

    Initial reworking to make matrix simpler

--- src/main/scala/com/blackcrowsys/crimson/matrix/Matrix.scala
+++ src/main/scala/com/blackcrowsys/crimson/matrix/Matrix.scala
@@ -1,15 +1,29 @@
-/**
-  * Copyright - see LICENCE
-  */
 package com.blackcrowsys.crimson.matrix
 
 object Matrix {
 
-  class Matrix[T](
-                   val contents: Array[T],
-                   val columns: Int
-                 ) {
-    def get(row: Int, col: Int): T = {
+  class Matrix(
+                val contents: Array[Double],
+                val columns: Int) {
+
+    val rows: Int = contents.length / columns
+
+    def *(scaler: Double): Matrix = {
+      val result = for (v <- this.contents) yield scaler * v
+      Matrix.create(result, this.columns)
+    }
+
+    def +(that: Matrix): Matrix = {
+      if (this.columns != that.columns || this.contents.length != that.contents.length)
+        throw new IllegalArgumentException("Matrix size mismatch")
+      val result =
+        for ((v1, v2) <- this.contents zip that.contents)
+          yield v1 + v2
+      Matrix.create(result, this.columns)
+    }
+
+
+    def get(row: Int, col: Int) = {
       if (col > columns) {
         throw new IllegalArgumentException("Column Index exceeds number of columns: " + col)
       }
@@ -22,12 +36,23 @@ object Matrix {
       contents(row * col - subtractor)
     }
 
+    def equals(that: Matrix): Boolean = {
+      if (this.columns != that.columns)
+        false
+      if (this.contents.length != that.contents.length)
+        false
+      for ((v1, v2) <- this.contents zip that.contents) {
+        if (v1 != v2)
+          return false
+      }
+      true
+    }
   }
 
-
-  def create(contents: Array[Double], columns: Int): Matrix[Double] = {
-    if (contents.length % columns != 0 || contents.length < columns)
+  def create(contents: Array[Double], cols: Int): Matrix = {
+    if (contents.length % cols != 0 || contents.length < cols)
       throw new IllegalArgumentException("Contents mismatch")
-    new Matrix[Double](contents, columns)
+    new Matrix(contents, cols)
   }
+
 }
--- src/test/scala/com/blackcrowsys/crimson/matrix/MatrixTests.scala
+++ src/test/scala/com/blackcrowsys/crimson/matrix/MatrixTests.scala
@@ -1,41 +1,65 @@
-/**
-  * Copyright - see LICENCE
-  */
 package com.blackcrowsys.crimson.matrix
 
-import org.scalatest.{FunSuite}
+import org.scalatest.FunSuite
 
 class MatrixTests extends FunSuite {
 
-  val x = Matrix.create(Array(12.2, 4, 7, 10), 2)
+  val x = Matrix.create(Array(12.5, 4.8, 3.4, 2), 2)
+  val y = Matrix.create(Array(2, 4, 6, 8), 2)
+  val z = Matrix.create(Array(2, 5), 1)
+  val expected = Matrix.create(Array(14.5, 8.8, 9.4, 10), 2)
 
-  test("A double matrix with 2 columns and 2 rows") {
-    val ele1 = x.get(1, 1)
-    assert(ele1 == 12.2)
+  test("it gets the right values by index") {
+    val e1 = x.get(1, 1)
+    assert(e1 == 12.5)
 
-    val ele2 = x.get(1, 2)
-    assert(ele2 == 4)
+    val e2 = x.get(1, 2)
+    assert(e2 == 4.8)
 
-    val ele3 = x.get(2, 1)
-    assert(ele3 == 7)
+    val e3 = x.get(2, 1)
+    assert(e3 == 3.4)
 
-    val ele4 = x.get(2, 2)
-    assert(ele4 == 10)
+    val e4 = x.get(2, 2)
+    assert(e4 == 2)
   }
 
-  assertThrows[IllegalArgumentException]{
-    val e = x.get(1,3)
+  assertThrows[IllegalArgumentException] {
+    val e = x.get(1, 3)
   }
 
-  assertThrows[IllegalArgumentException]{
+  assertThrows[IllegalArgumentException] {
     val e = x.get(3, 1)
   }
 
-  assertThrows[IllegalArgumentException]{
+  assertThrows[IllegalArgumentException] {
     val y = Matrix.create(Array(12.2, 4, 7), 2)
   }
 
-  assertThrows[IllegalArgumentException]{
+  assertThrows[IllegalArgumentException] {
     val y = Matrix.create(Array(12.1), 2)
   }
+
+  test("two matrix are same") {
+    assert(x.equals(x))
+  }
+
+  test("two matrix are not the same") {
+    assert(x.equals(y) == false)
+  }
+
+  test("it can add two matrix together") {
+    val actual = x + y
+    assert(actual.equals(expected))
+  }
+
+  assertThrows[IllegalArgumentException] {
+    x + z
+  }
+
+  test("scalar multiplication of a matrix") {
+    val actual = x * 2
+
+    assert(Matrix.create(Array(25, 9.6, 6.8, 4), 2).equals(actual))
+  }
+
 }
