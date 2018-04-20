38bb02a4b0829037af590d61ff5b570072f52218
ramindurs <ramindur.singh@blackcrowsys.com>
2018-04-20 13:49:50 +0530

    Added scala docs

--- src/main/scala/com/blackcrowsys/crimson/functions/Functions.scala
+++ src/main/scala/com/blackcrowsys/crimson/functions/Functions.scala
@@ -2,7 +2,17 @@ package com.blackcrowsys.crimson.functions
 
 import scala.math.exp
 
+/**
+  * A collection of mathemmatical functions that operate on Double or Matrices.
+  */
 object Functions {
+
+  /**
+    * The Sigmoid function on the Double.
+    *
+    * @param value the double on which to apply the sigmoid function
+    * @return the sigmoid
+    */
   def sigmoid(value: Double): Double = {
     1 / (1 + exp(-value))
   }
--- src/main/scala/com/blackcrowsys/crimson/ils/GradientDescent.scala
+++ src/main/scala/com/blackcrowsys/crimson/ils/GradientDescent.scala
@@ -3,8 +3,23 @@ package com.blackcrowsys.crimson.ils
 import com.blackcrowsys.crimson.common.Tolerence
 import com.blackcrowsys.crimson.matrix.Matrix.Matrix
 
+/**
+  * Implementation of the gradient descemnt method used for various forms of optimisation. See
+  * https://en.wikipedia.org/wiki/Gradient_descent for more information.
+  *
+  */
 class GradientDescent {
 
+  /**
+    * The gradient descent iteration on a matrix.
+    *
+    * @param derivative   the function that supplies the derivative, which should accept a matrix and return the derivative
+    *                     at that value
+    * @param initial      a matrix representing the starting point
+    * @param learningRate the learning rate
+    * @param tolerence    the tolerence , i.e. when the derivative is within this margin, we can stop iteration
+    * @return the final matrix at which point the derivative is minimised.
+    */
   def iterate(derivative: Matrix => Matrix, initial: Matrix, learningRate: Double, tolerence: Tolerence): Matrix = {
     val zero = initial.*(0)
     var derived: Matrix = derivative.apply(initial)
